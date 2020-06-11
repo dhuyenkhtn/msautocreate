fae39bd842a6369f79f98a0ab828e6ae3b62fe9f
Huyen Dan Phan <dhuyenkhtn@gmail.com>
2020-06-12 00:06:12 +0700

    updated

--- routers.js
+++ routers.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const routers = express.Router();
-const {getIndex} = require('./controllers');
+const {getIndex} = require('./controllers/IndexPage');
 const {createAccountMS} = require("./controllers/MSAccount");
 
 routers.get('/', (req, res) => {
