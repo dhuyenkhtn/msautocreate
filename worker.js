2fb78ef0b7eb9ecad73685e41a526105a46ae843
hannguyen263 <52794327+hannguyen263@users.noreply.github.com>
2020-07-30 11:42:48 +0700

    Update worker.js

--- worker.js
+++ worker.js
@@ -1,29 +1,29 @@
 const site_config = 
 {
     'notice': 'Automatic create Office 365 E3 - Please get activation code before create account!', 
-    'code_store_link': 'https://zalo.me/0763138666', 
+    'code_store_link': '#', 
     'line1': 'Auto Create Office',
     'line2': 'Sell Office 365 Enterprise',
-    'code_api_link': 'https://key-manager-msauto.herokuapp.com/active',
+    'code_api_link': 'http://api-keymanager.digibox.vn/',
     };
 
 const ms_config = 
 {
-    'tenant_id': '23e47019-e5cb-4a17-923e-ea538a788543',
-    'client_id': 'd8d4e572-10cb-4587-abab-568669e6e568',
-    'client_secret': 'iTW446RmKrR~-05HcEUo71L_nslqX_5N_L',
+    'tenant_id': '26e042f5-7b80-4db4-a060-d693d3b00c62',
+    'client_id': 'f6ffacef-b0a4-4859-9758-7b2eb3f3119d',
+    'client_secret': 'SV-X3EV2-5HSdxg9.8Q8_NEJ1tm1cP3qwh',
     'subscriptions': 
     [
         {
-        'name': 'Office 365 Enterprise E1',  //eg. Office 365 A1 for faculty
-        'skuId': '18181a46-0d4e-45cd-891e-60aabd171b4e',
+        'name': 'Office 365 Enterprise E5',  //eg. Office 365 A1 for faculty
+        'skuId': 'c42b9cae-ea4f-4ab7-9717-81576235ccac',
         } 
         //{
         //'name': '',
         //'skuId': ''
         //}
     ],
-    'domains': ['7upuk.onmicrosoft.com']
+    'domains': ['skywaydev.onmicrosoft.com']
 };
 
 const recaptcha_config = {
